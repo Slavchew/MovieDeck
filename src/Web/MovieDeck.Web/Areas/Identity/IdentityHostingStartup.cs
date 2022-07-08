0ed2ab83202ec7fe8b24b1e236fb50727cc9be84
Iceto04 <izetohr@gmail.com>
2022-07-08 12:52:04 +0300

    Scaffold and Create Custom Design for Login and Register Pages

---
+++ src/Web/MovieDeck.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieDeck.Data;
+using MovieDeck.Data.Models;
+
+[assembly: HostingStartup(typeof(MovieDeck.Web.Areas.Identity.IdentityHostingStartup))]
+namespace MovieDeck.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
