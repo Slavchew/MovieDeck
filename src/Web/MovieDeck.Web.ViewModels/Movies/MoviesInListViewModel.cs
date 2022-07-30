18530d25bb10de252d1ceb6b4c3059f9abe632ed
Iceto04 <izetohr@gmail.com>
2022-07-30 15:31:57 +0300

    Add movie info to main slider

--- src/Web/MovieDeck.Web.ViewModels/Movies/MoviesInListViewModel.cs
+++ src/Web/MovieDeck.Web.ViewModels/Movies/MoviesInListViewModel.cs
@@ -1,7 +1,5 @@
 ﻿namespace MovieDeck.Web.ViewModels.Movies
 {
-    using System;
-using System.Collections.Generic;
     using System.Linq;
 
     using AutoMapper;
@@ -9,41 +7,24 @@ using System.Collections.Generic;
     using MovieDeck.Common;
     using MovieDeck.Data.Models;
     using MovieDeck.Services.Mapping;
-    using MovieDeck.Web.ViewModels.Genres;
 
-    public class MovieViewModel : IMapFrom<Movie>, IHaveCustomMappings
+    public class MoviesInListViewModel : IMapFrom<Movie>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
 
-        public string Plot { get; set; }
-
-        public DateTime? ReleaseDate { get; set; }
-
-        public TimeSpan Runtime { get; set; }
+        public string PosterUrl { get; set; }
 
         public double AverageRating { get; set; }
 
-        public string PosterUrl { get; set; }
-
-        public string BackdropUrl { get; set; }
-
-        public List<GenreViewModel> Genres { get; set; }
-
-        public int GenresCount => Genres.Count;
-
         public void CreateMappings(IProfileExpression configuration)
         {
-            configuration.CreateMap<Movie, MovieViewModel>()
+            configuration.CreateMap<Movie, MoviesInListViewModel>()
                 .ForMember(x => x.PosterUrl, opt =>
                     opt.MapFrom(x => x.PosterPath.Contains("-") ?
                         $"/images/{GlobalConstants.MoviesImagesFolder}/" + x.PosterPath :
                         string.Format(GlobalConstants.RemoteImagesUrl, x.PosterPath)))
-                .ForMember(x => x.BackdropUrl, opt =>
-                    opt.MapFrom(x => x.BackdropPath.Contains("-") ?
-                        $"/images/{GlobalConstants.MoviesImagesFolder}/" + x.BackdropPath :
-                        string.Format(GlobalConstants.RemoteImagesUrl, x.BackdropPath)))
                 .ForMember(x => x.AverageRating, opt =>
                     opt.MapFrom(x => x.RatingsCount + x.Ratings.Count == 0
                         ? 0
---
+++ src/Web/MovieDeck.Web.ViewModels/Movies/MoviesListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieDeck.Web.ViewModels.Movies
+{
+    using System.Collections.Generic;
+
+    public class MoviesListViewModel
+    {
+        public IEnumerable<MoviesInListViewModel> Movies { get; set; }
+    }
+}
